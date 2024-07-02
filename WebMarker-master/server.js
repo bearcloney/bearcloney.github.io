e889014dc23e26deb40ed689b25e5b533d038b9b
bearcloney <51289114+bearcloney@users.noreply.github.com>
2024-07-02 21:56:41 +0800

    Delete WebMarker-master directory

--- WebMarker-master/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-var app = express();
-
-app.use(express.static("./"));
-
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
